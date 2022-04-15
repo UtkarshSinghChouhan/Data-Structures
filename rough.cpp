72ec673f48b19bb78410d79285d5fa71fa0f9c8e
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2022-04-15 14:04:32 +0530

    The bfs function complete

--- rough.cpp
+++ rough.cpp
@@ -7,25 +7,43 @@ private:
     int numNode;
     vector<int> *adjList;
     void dfsRec(int node, vector<int> &vis, vector<int> &storeDfs);
+
 public:
     Graph(int numNode);//
     void addEdge(int src, int des);//
-    vector<int> bfsOfGraph();
+    vector<int> bfsOfGraph();//
     vector<int> dfsOfGraph();
     void print();//
 };
 
+
 Graph :: Graph(int numNode)
 {
     this->numNode = numNode;
-    adjList = new vector<int>[numNode + 1];
+    adjList = new vector<int>[numNode + 1];         //as soon as we will be given the number of nodes(i.e. numNode) in the graph the adjacency-list of size (numNode+1) will be created
 }
+
 
 void Graph :: addEdge(int src, int des)
 {
     adjList[src].push_back(des);
     adjList[des].push_back(src);
 }
+
+
+//Funcion to print the adjacency-List of the graph
+void Graph :: print()
+{
+    for(int i = 0; i < (numNode + 1); i++)
+    {
+        for(int j : adjList[i])
+        {
+            cout << j <<" ";
+        }
+        cout<<endl;
+    }
+}
+
 
 vector<int> Graph :: bfsOfGraph()
 {
@@ -39,30 +57,54 @@ vector<int> Graph :: bfsOfGraph()
             queue<int> q;
             q.push(i);
             vis[i] = 1;
-             while(q.empty() == false)
-             {
-                 
-             }
+
+            while(q.empty() == false)
+            {
+                int node  = q.front();
+                q.pop();
+                storeBfs.push_back(node);
+
+                for(int adjNode : adjList[node])
+                {
+                    if(vis[adjNode] == 0)
+                    {
+                        q.push(adjNode);
+                        vis[adjNode] = 1;
+                    }
+                }
+            }
 
         }
     }
+
+    return storeBfs;
 }
 
 
-//Funcion to print the adjacency-List of the graph
-void Graph :: print()
-{
-    for(int i = 0; i < (numNode + 1); i++)
-    {
-        for(int j : adjList[i])
-        {
-            cout << j <<" ";
-        }
-    }
-}
+
 
 int main()
 {
-    
+    Graph g(7);
+
+    //Component C1 of the graph
+    g.addEdge(1,2);
+    g.addEdge(2,3);
+    g.addEdge(2,7);
+    g.addEdge(7,5);
+    g.addEdge(5,3);
+
+    //Component C2 of the graph
+    g.addEdge(4,6);
+
+    g.print();
+
+    //Printing the Bfs of the graph.
+    for(int value : g.bfsOfGraph())
+    {
+        cout << value << " ";
+    }
+    cout<<endl;
+
     return 0;
 }
